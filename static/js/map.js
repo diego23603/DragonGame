52fca2c3028bf60cae0643e0f3829961a8f0bd8e
diegocrgamer236 <37350554-diegocrgamer236@users.noreply.replit.com>
2024-11-04 20:54:32 +0000

    Agent step completed: Add interactive elements like NPCs or collectibles

--- static/js/map.js
+++ static/js/map.js
@@ -6,48 +6,68 @@ let collectibles = [];
 let npcs = [];
 let score = 0;
 let particles = [];
-let fireParticles = [];
-let smokeTrails = [];
-let dragonSilhouettes = [];
 
-const decorativeElements = [
-    { x: 100, y: 100, type: 'castle', size: 80 },
-    { x: 700, y: 500, type: 'castle', size: 80 },
-    { x: 200, y: 400, type: 'ancient_runes', size: 60 },
-    { x: 600, y: 200, type: 'dragon_statue', size: 70 }
-];
+// NPC configuration
+const NPC_TYPES = {
+    MERCHANT: {
+        sprite: '/static/images/dragons/green_dragon.svg',
+        message: 'Welcome traveler! Want to trade?'
+    },
+    GUARD: {
+        sprite: '/static/images/dragons/red_dragon.svg',
+        message: 'Keep the peace in Dragon World!'
+    },
+    WIZARD: {
+        sprite: '/static/images/dragons/blue_dragon.svg',
+        message: 'Seeking ancient dragon magic?'
+    }
+};
 
 function setup() {
     const canvas = createCanvas(mapSize.width, mapSize.height);
     canvas.parent('mapContainer');
     frameRate(60);
     
+    // Initialize NPCs
+    initializeNPCs();
+    
+    // Initialize collectibles
+    initializeCollectibles();
+}
+
+function initializeNPCs() {
+    const npcPositions = [
+        { x: 100, y: 100, type: 'MERCHANT' },
+        { x: 700, y: 500, type: 'GUARD' },
+        { x: 400, y: 200, type: 'WIZARD' }
+    ];
+    
+    npcPositions.forEach(pos => {
+        const npcType = NPC_TYPES[pos.type];
+        loadImage(npcType.sprite, img => {
+            npcs.push({
+                x: pos.x,
+                y: pos.y,
+                sprite: img,
+                type: pos.type,
+                message: npcType.message,
+                interactionRadius: 60,
+                lastInteraction: 0
+            });
+        });
+    });
+}
+
+function initializeCollectibles() {
+    const collectibleTypes = ['dragon_egg', 'dragon_scale', 'magic_crystal'];
     for (let i = 0; i < 5; i++) {
         collectibles.push({
             x: random(50, mapSize.width - 50),
             y: random(50, mapSize.height - 50),
+            type: random(collectibleTypes),
             collected: false,
-            type: random(['dragon_egg', 'dragon_scale', 'dragon_chest']),
-            glowPhase: random(TWO_PI),
-            floatOffset: random(TWO_PI),
-            runeRotation: 0
-        });
-    }
-    
-    for (let i = 0; i < 3; i++) {
-        dragonSilhouettes.push({
-            x: random(width),
-            y: random(height),
-            size: random(100, 200),
-            speed: random(0.5, 1.5),
-            offset: random(TWO_PI)
-        });
-    }
-    
-    if (!characterSprite) {
-        const defaultDragon = '/static/images/dragons/red_dragon.svg';
-        loadImage(defaultDragon, img => {
-            characterSprite = img;
+            value: random([10, 20, 30]),
+            pulsePhase: random(TWO_PI)
         });
     }
 }
@@ -60,331 +80,186 @@ function draw() {
         blue(bgColor) * 0.7
     ));
     
-    drawDragonScales();
-    drawDragonSilhouettes();
-    drawDragonGrid();
-    drawDecorativeElements();
-    updateFireParticles();
-    updateSmokeTrails();
+    drawGrid();
+    drawNPCs();
     drawCollectibles();
     
+    // Draw other players
     users.forEach((user, id) => {
-        drawCharacter(user.x, user.y, user.dragonSprite || characterSprite);
+        drawCharacter(user.x, user.y, user.dragonSprite, user.username);
     });
     
-    drawCharacter(myPosition.x, myPosition.y, characterSprite);
-    updateScore();
+    // Draw current player
+    drawCharacter(myPosition.x, myPosition.y, characterSprite, 'You');
+    
+    // Check interactions
+    checkNPCInteractions();
     checkCollectibles();
+    
+    // Draw score
+    updateScore();
 }
 
-function drawDragonScales() {
-    push();
-    noFill();
-    for (let x = 0; x < width; x += 30) {
-        for (let y = 0; y < height; y += 30) {
-            let shimmerAmount = sin(frameCount * 0.02 + x * 0.1 + y * 0.1) * 0.5 + 0.5;
-            stroke(255, 100, 50, 20 * shimmerAmount);
-            arc(x, y, 25, 25, 0, PI * 0.8);
-        }
-    }
-    pop();
-}
-
-function drawDragonSilhouettes() {
-    push();
-    noStroke();
-    dragonSilhouettes.forEach(dragon => {
-        dragon.x += dragon.speed;
-        if (dragon.x > width + dragon.size) dragon.x = -dragon.size;
-        
-        let y = dragon.y + sin(frameCount * 0.02 + dragon.offset) * 20;
-        fill(255, 100, 50, 30);
-        
-        beginShape();
-        vertex(dragon.x, y);
-        vertex(dragon.x + dragon.size * 0.8, y - dragon.size * 0.2);
-        vertex(dragon.x + dragon.size, y);
-        vertex(dragon.x + dragon.size * 0.8, y + dragon.size * 0.2);
-        endShape(CLOSE);
-    });
-    pop();
-}
-
-function drawDragonGrid() {
-    push();
-    strokeWeight(2);
+function drawGrid() {
+    stroke(255, 255, 255, 20);
+    strokeWeight(1);
     for (let x = 0; x < width; x += 50) {
-        for (let y = 0; y < height; y += 50) {
-            stroke(255, 100, 50, 10 + sin(frameCount * 0.02 + x * 0.1 + y * 0.1) * 5);
-            noFill();
-            arc(x, y, 40, 40, 0, PI/2);
-        }
+        line(x, 0, x, height);
     }
-    pop();
+    for (let y = 0; y < height; y += 50) {
+        line(0, y, width, y);
+    }
 }
 
-function drawDecorativeElements() {
-    decorativeElements.forEach(elem => {
+function drawCharacter(x, y, dragonSprite = characterSprite, playerName = '') {
+    if (dragonSprite) {
         push();
-        if (elem.type === 'castle') {
-            drawCastle(elem);
-        } else if (elem.type === 'ancient_runes') {
-            drawRunes(elem);
-        } else if (elem.type === 'dragon_statue') {
-            drawDragonStatue(elem);
+        imageMode(CENTER);
+        
+        // Draw shadow
+        noStroke();
+        fill(0, 0, 0, 30);
+        ellipse(x, y + 24, 40, 20);
+        
+        // Draw character
+        image(dragonSprite, x, y, 48, 48);
+        
+        // Draw name
+        if (playerName) {
+            drawPlayerName(x, y, playerName);
         }
-        pop();
-    });
-}
-
-function drawCastle(elem) {
-    fill(100, 50, 50, 100);
-    rect(elem.x - elem.size/2, elem.y - elem.size, elem.size, elem.size);
-    for(let i = 0; i < 4; i++) {
-        rect(elem.x - elem.size/2 + i*elem.size/4, 
-             elem.y - elem.size - 10, 
-             elem.size/6, 20);
-    }
-    fill(255, 200, 0, 50 + sin(frameCount * 0.05) * 30);
-    for(let i = 0; i < 3; i++) {
-        rect(elem.x - elem.size/3 + i*elem.size/3,
-             elem.y - elem.size*0.7,
-             elem.size/8, elem.size/8);
-    }
-}
-
-function drawRunes(elem) {
-    push();
-    translate(elem.x, elem.y);
-    rotate(frameCount * 0.01);
-    
-    noFill();
-    for(let i = 0; i < 3; i++) {
-        stroke(255, 100, 50, 50 - i * 10);
-        circle(0, 0, elem.size + i * 10);
-    }
-    
-    stroke(255, 150, 50, 100);
-    for(let i = 0; i < 8; i++) {
-        push();
-        rotate(i * TWO_PI/8);
-        line(0, elem.size/3, 0, elem.size/2);
+        
         pop();
     }
-    pop();
 }
 
-function drawDragonStatue(elem) {
+function drawPlayerName(x, y, name) {
     push();
-    translate(elem.x, elem.y);
-    
-    fill(80, 40, 40);
-    rect(-elem.size/3, -elem.size/4, elem.size*2/3, elem.size/4);
-    
-    fill(100, 50, 50);
-    beginShape();
-    vertex(0, -elem.size/2);
-    vertex(elem.size/3, -elem.size*3/4);
-    vertex(0, -elem.size);
-    vertex(-elem.size/3, -elem.size*3/4);
-    endShape(CLOSE);
-    
-    noFill();
-    stroke(255, 100, 50, 30 + sin(frameCount * 0.05) * 20);
-    circle(0, -elem.size/2, elem.size/2);
+    fill(255);
+    noStroke();
+    textSize(14);
+    textAlign(CENTER);
+    text(name, x, y - 40);
     pop();
 }
 
-function updateFireParticles() {
-    if (frameCount % 3 === 0) {
-        fireParticles.push({
-            x: random(width),
-            y: height + 10,
-            vx: random(-2, 2),
-            vy: random(-8, -4),
-            size: random(5, 15),
-            life: 255,
-            hue: random(0, 40)
-        });
-    }
-    
-    for (let i = fireParticles.length - 1; i >= 0; i--) {
-        let p = fireParticles[i];
-        p.x += p.vx;
-        p.y += p.vy;
-        p.vy *= 0.98;
-        p.life -= 4;
-        p.size *= 0.98;
-        
-        noStroke();
-        let warmth = map(p.life, 0, 255, 0, 1);
-        fill(p.hue + 10, 100, 50, p.life);
-        circle(p.x + sin(frameCount * 0.1) * 2, p.y, p.size);
-        
-        if (p.life <= 0) {
-            fireParticles.splice(i, 1);
-        }
-    }
-}
-
-function updateSmokeTrails() {
-    if (frameCount % 4 === 0) {
-        smokeTrails.push({
-            x: myPosition.x,
-            y: myPosition.y,
-            size: random(10, 20),
-            life: 255,
-            vx: random(-0.5, 0.5),
-            vy: random(-1, -0.5)
-        });
-    }
-    
-    for (let i = smokeTrails.length - 1; i >= 0; i--) {
-        let p = smokeTrails[i];
-        p.x += p.vx;
-        p.y += p.vy;
-        p.life -= 5;
-        p.size *= 1.02;
-        
-        noStroke();
-        fill(200, p.life * 0.2);
-        circle(p.x, p.y, p.size);
-        
-        if (p.life <= 0) {
-            smokeTrails.splice(i, 1);
-        }
-    }
-}
-
-function drawCollectibles() {
-    collectibles.forEach((c, index) => {
-        if (!c.collected) {
+function drawNPCs() {
+    npcs.forEach(npc => {
+        if (npc.sprite) {
             push();
-            translate(c.x, c.y + sin(frameCount * 0.05 + c.floatOffset) * 5);
+            imageMode(CENTER);
             
-            c.runeRotation += 0.01;
-            drawRuneCircle(c);
+            // Draw interaction radius
+            noFill();
+            stroke(255, 255, 255, 50 + sin(frameCount * 0.05) * 20);
+            circle(npc.x, npc.y, npc.interactionRadius * 2);
             
-            let glowSize = 30 + sin(frameCount * 0.05 + c.glowPhase) * 8;
-            for (let i = 0; i < 3; i++) {
-                noFill();
-                stroke(255, 150, 50, 40 - i * 10);
-                circle(0, 0, glowSize + i * 10);
-            }
+            // Draw NPC
+            image(npc.sprite, npc.x, npc.y, 48, 48);
             
-            if (c.type === 'dragon_egg') {
-                drawDragonEgg();
-            } else if (c.type === 'dragon_scale') {
-                drawDragonScale();
-            } else if (c.type === 'dragon_chest') {
-                drawDragonChest();
-            }
+            // Draw type label
+            fill(255);
+            noStroke();
+            textAlign(CENTER);
+            textSize(12);
+            text(npc.type, npc.x, npc.y - 35);
+            
             pop();
         }
     });
 }
 
-function drawRuneCircle(collectible) {
-    push();
-    rotate(collectible.runeRotation);
-    noFill();
-    stroke(255, 150, 50, 50);
-    circle(0, 0, 40);
-    
-    for (let i = 0; i < 6; i++) {
-        push();
-        rotate(i * TWO_PI/6);
-        stroke(255, 150, 50, 70);
-        line(0, 15, 0, 20);
-        pop();
-    }
-    pop();
+function drawCollectibles() {
+    collectibles.forEach(c => {
+        if (!c.collected) {
+            push();
+            translate(c.x, c.y);
+            
+            // Pulse effect
+            let pulseSize = 20 + sin(frameCount * 0.05 + c.pulsePhase) * 5;
+            
+            // Glow effect
+            noFill();
+            for (let i = 0; i < 3; i++) {
+                stroke(255, 200, 0, 50 - i * 15);
+                circle(0, 0, pulseSize + i * 10);
+            }
+            
+            // Draw collectible
+            fill(255, 200, 0);
+            noStroke();
+            circle(0, 0, pulseSize);
+            
+            // Draw type label
+            fill(255);
+            noStroke();
+            textAlign(CENTER);
+            textSize(10);
+            text(c.type, 0, pulseSize + 15);
+            
+            pop();
+        }
+    });
 }
 
-function drawDragonEgg() {
-    fill(255, 200, 150);
-    ellipse(0, 0, 25, 35);
-    
-    stroke(255, 150, 50);
-    noFill();
-    for (let i = 0; i < 3; i++) {
-        arc(0, -5 + i * 5, 20 - i * 5, 10, 0, PI);
-    }
-}
-
-function drawDragonScale() {
-    push();
-    rotate(sin(frameCount * 0.05) * 0.2);
-    
-    fill(255, 150, 50);
-    beginShape();
-    vertex(0, -15);
-    vertex(12, 0);
-    vertex(0, 15);
-    vertex(-12, 0);
-    endShape(CLOSE);
-    
-    noFill();
-    stroke(255, 200, 150, 100);
-    beginShape();
-    vertex(0, -12);
-    vertex(10, 0);
-    vertex(0, 12);
-    vertex(-10, 0);
-    endShape(CLOSE);
-    pop();
-}
-
-function drawDragonChest() {
-    fill(139, 69, 19);
-    rect(-15, -12, 30, 24);
-    
-    fill(101, 67, 33);
-    rect(-15, -12, 30, 8);
-    
-    fill(255, 215, 0);
-    rect(-16, -4, 32, 2);
-    
-    fill(218, 165, 32);
-    rect(-2, -8, 4, 8);
+function checkNPCInteractions() {
+    npcs.forEach(npc => {
+        const d = dist(myPosition.x, myPosition.y, npc.x, npc.y);
+        if (d < npc.interactionRadius) {
+            // Only show message every 3 seconds
+            if (millis() - npc.lastInteraction > 3000) {
+                // Display message above NPC
+                push();
+                fill(255);
+                noStroke();
+                textAlign(CENTER);
+                textSize(12);
+                text(npc.message, npc.x, npc.y - 50);
+                pop();
+                
+                npc.lastInteraction = millis();
+            }
+        }
+    });
 }
 
 function checkCollectibles() {
-    collectibles.forEach((c, index) => {
+    collectibles.forEach(c => {
         if (!c.collected && dist(myPosition.x, myPosition.y, c.x, c.y) < 30) {
             c.collected = true;
-            score += 10;
-            document.getElementById('currentScore').textContent = score;
+            score += c.value;
             
-            for (let i = 0; i < 20; i++) {
+            // Create particles
+            for (let i = 0; i < 10; i++) {
                 particles.push({
                     x: c.x,
                     y: c.y,
                     vx: random(-3, 3),
                     vy: random(-3, 3),
-                    life: 255,
-                    color: color(255, 200, 50)
+                    life: 255
                 });
             }
             
+            // Update score display
+            document.getElementById('currentScore').textContent = score;
+            
+            // Emit collection event
             if (socket) {
-                socket.emit('collectible_collected', {
-                    x: c.x,
-                    y: c.y
-                });
+                socket.emit('collectible_collected', { x: c.x, y: c.y });
             }
         }
     });
     
+    // Update particles
     for (let i = particles.length - 1; i >= 0; i--) {
         let p = particles[i];
         p.x += p.vx;
         p.y += p.vy;
         p.life -= 5;
         
+        // Draw particle
         noStroke();
-        p.color.setAlpha(p.life);
-        fill(p.color);
+        fill(255, 200, 0, p.life);
         circle(p.x, p.y, 5);
         
         if (p.life <= 0) {
@@ -393,46 +268,17 @@ function checkCollectibles() {
     }
 }
 
-function drawCharacter(x, y, dragonSprite = characterSprite) {
-    if (dragonSprite) {
-        push();
-        imageMode(CENTER);
-        
-        drawingContext.filter = 'url(#heat-distortion)';
-        
-        noFill();
-        for (let i = 0; i < 3; i++) {
-            stroke(255, 100, 100, (30 - i*10) + sin(frameCount * 0.1) * 20);
-            circle(x, y, 40 + i*10);
-        }
-        
-        noStroke();
-        fill(0, 0, 0, 30);
-        ellipse(x, y + 24, 40, 20);
-        
-        image(dragonSprite, x, y, 48, 48);
-        
-        drawingContext.filter = 'none';
-        pop();
-    }
-}
-
 function updateScore() {
     fill(255, 150, 50);
     noStroke();
     textSize(20);
     textAlign(LEFT, TOP);
-    text(`Score: ${score}`, 10, 20);
+    text(`Score: ${score}`, 10, 10);
 }
 
 function keyPressed() {
-    const step = 10;
+    const step = 5;
     let moved = false;
-    
-    if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW || 
-        keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
-        event.preventDefault();
-    }
     
     if (keyCode === LEFT_ARROW) {
         myPosition.x = max(24, myPosition.x - step);
--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,11 +1,20 @@
 const socket = io();
+let currentUsername = '';
 
 socket.on('connect', () => {
     console.log('Connected to server');
-    if (selectedDragon) {
-        socket.emit('dragon_selected', {
-            dragonId: selectedDragon.id
-        });
+    // Load saved auth token
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        socket.emit('authenticate', { token });
+    }
+});
+
+socket.on('authenticated', (data) => {
+    currentUsername = data.username;
+    myPosition = data.position || { x: 400, y: 300 };
+    if (data.selectedDragon) {
+        selectDragon(data.selectedDragon);
     }
 });
 
@@ -13,6 +22,7 @@ socket.on('user_moved', (data) => {
     if (data.userId !== socket.id) {
         const dragon = availableDragons.find(d => d.id === data.dragonId);
         users.set(data.userId, {
+            username: data.username,
             x: data.x,
             y: data.y,
             dragonSprite: dragon ? loadImage(dragon.sprite) : null
@@ -37,21 +47,41 @@ socket.on('user_connected', (data) => {
     console.log('User connected:', data);
 });
 
-socket.on('collectibles_state', (state) => {
-    Object.entries(state).forEach(([id, collected]) => {
-        const [x, y] = id.split('_').map(Number);
-        const collectible = collectibles.find(c => c.x === x && c.y === y);
-        if (collectible) {
-            collectible.collected = collected;
-        }
-    });
+socket.on('user_disconnected', (data) => {
+    users.delete(data.userId);
 });
 
-socket.on('collectible_collected', (data) => {
-    const collectible = collectibles.find(c => 
-        c.x === data.x && c.y === data.y && !c.collected
-    );
-    if (collectible) {
-        collectible.collected = true;
+function emitPosition(x, y, dragonId) {
+    if (socket && socket.connected) {
+        socket.emit('position_update', {
+            x,
+            y,
+            dragonId,
+            username: currentUsername
+        });
     }
-});
+}
+
+// Update keyPressed function in map.js to use the new emitPosition function
+function keyPressed() {
+    const step = 10;
+    let moved = false;
+    
+    if (keyCode === LEFT_ARROW) {
+        myPosition.x = max(24, myPosition.x - step);
+        moved = true;
+    } else if (keyCode === RIGHT_ARROW) {
+        myPosition.x = min(mapSize.width - 24, myPosition.x + step);
+        moved = true;
+    } else if (keyCode === UP_ARROW) {
+        myPosition.y = max(24, myPosition.y - step);
+        moved = true;
+    } else if (keyCode === DOWN_ARROW) {
+        myPosition.y = min(mapSize.height - 24, myPosition.y + step);
+        moved = true;
+    }
+    
+    if (moved) {
+        emitPosition(myPosition.x, myPosition.y, selectedDragon?.id);
+    }
+}
